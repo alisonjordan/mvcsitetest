17fce3150c1adad31620219f10201cc9ce3b4a71
alisonjordan <alisonjor@gmail.com>
2023-08-24 14:26:06 -0300

    folder creation and friendly url addition

---
+++ view/compile/e1d197bfe036e1992ada83347191b611f9f12f5f_0.file.index.tpl.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 3.1.46, created on 2023-08-23 17:55:43
+  from 'C:\xampp\htdocs\testesitemvc\view\index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.46',
+  'unifunc' => 'content_64e62bff78d5e7_92116506',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e1d197bfe036e1992ada83347191b611f9f12f5f' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\testesitemvc\\view\\index.tpl',
+      1 => 1692806135,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_64e62bff78d5e7_92116506 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+
+
+<body>
+
+Algo aqui
+
+<h2><?php echo $_smarty_tpl->tpl_vars['NAME']->value;?>
+</h2>
+
+</body>
+
+
+</html><?php }
+}
