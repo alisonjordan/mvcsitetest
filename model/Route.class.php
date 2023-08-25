e410377b3ad46689a6a96dbc33e926e1d72cb417
alisonjordan <alisonjor@gmail.com>
2023-08-25 12:59:36 -0300

    addition of site root, site theme and page clients functions

--- model/Route.class.php
+++ model/Route.class.php
@@ -2,6 +2,8 @@
 Class Route{
 
     public static $urls;
+    private static $folder_controller = 'controller';
+    private static $folder_view = 'view';
 
     static function get_SiteHome(){
         return Config::SITE_URL . '/' . Config::SITE_FOLDER;
@@ -9,6 +11,14 @@ Class Route{
 
     static function get_SiteRoot(){
         return $_SERVER['DOCUMENT_ROOT'] . '/' . Config::SITE_FOLDER;
+    }
+
+    static function get_SiteTheme(){
+        return self::get_SiteHome() . '/' . self::$folder_view;
+    }
+
+    static function page_Clients(){
+        return self::get_SiteHome() . '/clients' . self::$folder_view;
     }
 
     static function get_Page(){
