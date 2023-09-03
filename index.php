0d71b07ff268b3ce8c6c2204293796a76b08d6b2
alisonjordan <alisonjor@gmail.com>
2023-09-02 21:08:09 -0300

    minor updates

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require './lib/autoload.php';
 
 $smarty = new Template();
+$categories = new Categories();
+$categories->GetCategories();
 
 //values for template
 
@@ -10,6 +12,7 @@ $smarty->assign('GET_SITE_NAME',Config::SITE_NAME);
 
 $smarty->assign('GET_THEME',Route::get_SiteTheme());
 $smarty->assign('GET_HOME',Route::get_SiteHome());
+$smarty->assign('CATEGORIES', $categories->GetItems());
 $smarty->assign('GET_PAGE_PRODUCTS',Route::page_Products());
 $smarty->assign('GET_PAGE_ABOUT',Route::page_About());
 
