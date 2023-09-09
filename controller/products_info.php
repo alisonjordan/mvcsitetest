d21fd0041df44cd8169cf6d5cb4c89d56a9e80e4
alisonjordan <alisonjor@gmail.com>
2023-09-09 02:19:25 -0300

    minor updates

--- controller/products_info.php
+++ controller/products_info.php
@@ -10,6 +10,22 @@ $products->GetProductsID(Route::$urls[1]);
 $image = new ProductsImages();
 $image->GetImagesPRO(Route::$urls[1]);
 
+/*
+$ID = Route::$urls[1];
+foreach ($products->GetItems() as $pro) {
+	$_SESSION['PRO'][$ID]['ID'] = $pro['pro_id'];
+	$_SESSION['PRO'][$ID]['NAME'] = $pro['pro_name'];
+    $_SESSION['PRO'][$ID]['PRICE'] = $pro['pro_price'];
+    $_SESSION['PRO'][$ID]['PRICE_US'] = $pro['pro_price_us'];
+    $_SESSION['PRO'][$ID]['WEIGHT'] = $pro['pro_weight'];
+    $_SESSION['PRO'][$ID]['QTT'] = 1;
+    $_SESSION['PRO'][$ID]['IMAGE'] = $pro['pro_image'];
+    $_SESSION['PRO'][$ID]['LINK'] = 'sssslink';
+    $ID++;
+}
+
+*/
+
 
 
 
