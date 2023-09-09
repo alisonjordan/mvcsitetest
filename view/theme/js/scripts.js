30b7c384f50e5458f379346b0120c29eb879ea0a
alisonjordan <alisonjor@gmail.com>
2023-09-09 02:15:56 -0300

    addition of freight calculation

--- view/theme/js/scripts.js
+++ view/theme/js/scripts.js
@@ -8,3 +8,36 @@
 $(document).ready(function() {
     var $easyzoom = $('.easyzoom').easyZoom();
 });
+
+
+
+$(document).ready(function(){    
+
+   // validar frete
+     $('#search_freight').click(function(){  
+        
+      var CEP_CLIENT = $('#cep_freight').val();
+      var WEIGHT_FREIGHT = $('#weight_freight').val();
+       
+        if (CEP_CLIENT.length !== 8 ) {
+        alert('Enter your CEP code correctly, 8 digits and no dashes or periods.');  
+         $('#freight').addClass(' text-center text-danger');
+         $('#freight').html('<b>Enter your CEP code correctly, 8 digits and no dashes or periods.</b>');
+        $('#cep_freight').focus();
+        } else {
+     
+       
+        $('#freight').html('<i class="fa-solid fa-rotate fa-spin fa-lg"></i>');
+        $('#freight').addClass(' text-center text-danger');
+      
+        
+       
+        $('#freight').load('controller/freight.php?cepclient='+CEP_CLIENT+'&weightfreight='+WEIGHT_FREIGHT);
+ 
+ } 
+      
+ 
+    });
+    
+   
+} );
